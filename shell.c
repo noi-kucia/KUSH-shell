e7743795e73a8d76db4b507d9f8a7df6ddf61787
KUCIA <noiszewczyk@gmail.com>
2024-11-17 23:58:13 +0100

    greeting update

--- shell.c
+++ shell.c
@@ -10,6 +10,9 @@ TODO: anything...
 #include <stdint.h>
 #include <string.h>
 #include <stdarg.h>
+#include <wchar.h>
+#include <string.h>
+#include <locale.h>
 
 // helper structures and objects
 typedef struct{
@@ -38,20 +41,26 @@ void cprintnl(const char* text, const char* color){
 
 void print_greetings(void){
     printf(" \n");
-    cprintnl(R"edge( ______________________________________________________   )edge", Colors.PURPLE);
-    cprintnl(R"edge(/_____/_____/_____/_____/_____/_____/_____/_____/_____/   )edge", Colors.PURPLE);
-    cprintnl(R"edge( _      _____  / /________  ____ ___  ___     / /_____  _ )edge", Colors.PURPLE);
-    cprintnl(R"edge(| | /| / / _ \/ / ___/ __ \/ __ `__ \/ _ \   / __/ __ \(_))edge", Colors.PURPLE);
-    cprintnl(R"edge(| |/ |/ /  __/ / /__/ /_/ / / / / / /  __/  / /_/ /_/ /   )edge", Colors.PURPLE);
-    cprintnl(R"edge(|__/|__/\___/_/\___/\____/_/ /_/ /_/\___/   \__/\____(_)  )edge", Colors.PURPLE);
-    cprintnl(R"edge(                   / //_/ ___// / / /                     )edge", Colors.PURPLE);
-    cprintnl(R"edge(                  / ,<  \__ \/ /_/ /                      )edge", Colors.PURPLE);
-    cprintnl(R"edge(                 / /| |___/ / __  /                       )edge", Colors.PURPLE);
-    cprintnl(R"edge(________________/_/_|_/____/_/_/_/_______________         )edge", Colors.PURPLE);
-    cprintnl(R"edge(/_____/_____/_____/_____/_____/_____/_____/_____/         )edge", Colors.PURPLE);
+    cprintnl(R"edge(____    __    ____  _______  __        ______   ______   .___  ___.  _______ )edge", Colors.PURPLE);
+    cprintnl(R"edge(\   \  /  \  /   / |   ____||  |      /      | /  __  \  |   \/   | |   ____|)edge", Colors.PURPLE);
+    cprintnl(R"edge( \   \/    \/   /  |  |__   |  |     |  ,----'|  |  |  | |  \  /  | |  |__   )edge", Colors.PURPLE);
+    cprintnl(R"edge(  \            /   |   __|  |  |     |  |     |  |  |  | |  |\/|  | |   __|  )edge", Colors.PURPLE);
+    cprintnl(R"edge(   \    /\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____ )edge", Colors.PURPLE);
+    cprintnl(R"edge(    \__/  \__/     |_______||_______| \______| \______/  |__|  |__| |_______|)edge", Colors.PURPLE);
+    cprintnl(R"edge(                                                                             )edge", Colors.PURPLE);
+    cprintnl(R"edge(.___________.  ______         __  ___      _______. __    __       __        )edge", Colors.PURPLE);
+    cprintnl(R"edge(|           | /  __  \       |  |/  /     /       ||  |  |  |     |  |       )edge", Colors.PURPLE);
+    cprintnl(R"edge(`---|  |----`|  |  |  |      |  '  /     |   (----`|  |__|  |     |  |       )edge", Colors.PURPLE);
+    cprintnl(R"edge(    |  |     |  |  |  |      |    <       \   \    |   __   |     |  |       )edge", Colors.PURPLE);
+    cprintnl(R"edge(    |  |     |  `--'  |      |  .  \  .----)   |   |  |  |  |     |__|       )edge", Colors.PURPLE);
+    cprintnl(R"edge(    |__|      \______/       |__|\__\ |_______/    |__|  |__|     (__)       )edge", Colors.PURPLE);
+    cprintnl(R"edge(                                                                             )edge", Colors.PURPLE);
+    cprintnl(R"edge(-----------------------------------------------------------------------------)edge", Colors.PURPLE);
+    printf("\n");
 }
 
 int main(void){
+    setlocale(LC_ALL, "");  // Set locale to the user's default
     print_greetings();
     return 0;
 }
