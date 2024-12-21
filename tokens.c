32a453478d77cbfb63ffde18158aa2c96f922413
KUCIA <noiszewczyk@gmail.com>
2024-12-21 16:41:21 +0100

    defined a few types and structures

--- tokens.c
+++ tokens.c
@@ -4,19 +4,35 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 extern void error_message(const char* text);
 
-struct token {
-
+// defining types
+typedef char tchar_t;  // type of token character
+enum token_types{
+    token_unkown,
+    token_command,
+    token_argument,
+    token_semicolon,
+    token_pipe,
+    token_inredir,
+    token_outredir
 };
 
-void* tokenize(const char* command) {
+
+struct token {
+    enum token_types type;
+    const tchar_t* src;
+    uint16_t length;
+};
+
+struct token** tokenize(const char* command) {
     /* Tokenize the command and returns a pointer to the array of tokens, that ends with special end-token
      * returns nullptr when unable to parse or allocate enough memory */
 
     size_t array_size = 32;
-    struct token* tokens[array_size];
+    struct token* tokens = malloc(sizeof(struct token) * array_size);
 
-    return nullptr;
+    return tokens;
 };
