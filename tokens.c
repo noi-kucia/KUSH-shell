a49e4b6aeca89b67ec9b8530a2cf7fd6e3f8b260
KUCIA <noiszewczyk@gmail.com>
2024-12-21 17:06:42 +0100

    changed the lexer structure

--- tokens.c
+++ tokens.c
@@ -11,6 +11,7 @@ extern void error_message(const char* text);
 // defining types
 typedef char tchar_t;  // type of token character
 enum token_types{
+    token_error,
     token_unkown,
     token_command,
     token_argument,
@@ -21,18 +22,24 @@ enum token_types{
 };
 
 
-struct token {
+struct token{
     enum token_types type;
-    const tchar_t* src;
+    const tchar_t *src;
     uint16_t length;
 };
 
-struct token** tokenize(const char* command) {
-    /* Tokenize the command and returns a pointer to the array of tokens, that ends with special end-token
-     * returns nullptr when unable to parse or allocate enough memory */
+struct token next_token(const tchar_t *command) {
+    /* Part of the lexer functionality serves for receiving one token.
+     * The only argument is a pointer to place to start scanning from.
+     * Returns a token struct object, and if for some reason it's impossible,
+     * the type will be set to error (empty string or allocation error).
+     * If some unknown sequence or character is found, returns a token with token_unknown type.
+     *
+     * Note that token.src could not match the argument pointer, cuz white characters can be omitted.
+     */
 
-    size_t array_size = 32;
-    struct token* tokens = malloc(sizeof(struct token) * array_size);
+    struct token token = {token_error, command, 1};
+    tchar_t *src = command;
 
-    return tokens;
+    return token;
 };
