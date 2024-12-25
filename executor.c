e43ed0ad15616a6e8d40e952665382f1fcb97248
KUCIA <noiszewczyk@gmail.com>
2024-12-25 18:24:11 +0100

    init

---
+++ executor.c
@@ -0,0 +1,8 @@
+//
+// Created by kucia on 12/25/24.
+// Provides methods to execute sequences of tokens.
+//
+
+#include "executor.h"
+
+// TODO:
---
+++ executor.h
@@ -0,0 +1,8 @@
+//
+// Created by kucia on 12/25/24.
+//
+
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+#endif //EXECUTOR_H
