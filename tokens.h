ff4778cb4a5dc3e35d2d3ac04418b4d56b4f3616
KUCIA <noiszewczyk@gmail.com>
2025-01-05 12:39:17 +0100

    add a header

--- tokens.h
+++ tokens.h
@@ -33,6 +33,7 @@ struct token{
 
 struct token next_token_safe(const tchar_t *);
 struct token **get_tokens_safe(const tchar_t *);
+struct token **get_pipe_segment(struct token **prev_segment);
 
 
 
