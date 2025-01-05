1a711241b763a48ed72d6fdba735a968a414a210
KUCIA <noiszewczyk@gmail.com>
2025-01-05 13:43:28 +0100

    fix a bug when semicolon or a pipe in command term was recognized as a forbiden character

--- tokens.c
+++ tokens.c
@@ -91,7 +91,7 @@ struct token next_token_safe(const tchar_t *command) {
 
                     }
                 }
-                else if (strchr(white_characters, *src)!=NULL) break;
+                else if (strchr(white_characters, *src)!=NULL || strchr("|;", *src)!=NULL) break;
                 else {
                     error_emph_prefix("Forbiden character has been found - ", token.src, token.length, token.length+1);
                     token.type = token_error;
